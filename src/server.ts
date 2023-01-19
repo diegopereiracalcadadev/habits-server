7a4d9c771d25ec1e7b21ecac74e502eaf347abcb
Diego Pereira <diegopereiracalcada@gmail.com>
2023-01-18 23:44:35 -0300

    Installed Prisma + Created Habits Table + Fastify CORS

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,25 @@
 import Fastify from 'fastify'
+import cors from '@fastify/cors'
+
+import {PrismaClient}  from '@prisma/client'
 
 const app = Fastify()
+const prisma = new PrismaClient();
 
-app.get('/', () => {
-    return "Root route Fastify App  !! !!"
+app.register(cors); // It's possible to define allowed origins
+
+app.get('/', async () => {
+    const habits = await prisma.habit.findMany({
+        where: {
+            title: {
+                startsWith: 'Drink'
+            }       
+        }
+    });
+
+    return habits 
 })
 
 app.listen({
     port: 3333
-})
+}).then(() => console.log("Server running..."))
